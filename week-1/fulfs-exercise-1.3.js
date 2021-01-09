ded9bc2cde814a963cba7c680bfe7b49d84c1c6e
Your Name <you@example.com>
2021-01-08 22:32:03 -0600

    commit finished exercise

--- week-1/fulfs-exercise-1.3.js
+++ week-1/fulfs-exercise-1.3.js
@@ -1,7 +1,7 @@
 /*
 ============================================
 ; Title: Exercise 1.3
-; Author: Perry Fulfs
+; Author: Richard Krasso
 ; Date: 08 Janruary 2021
 ; Modified By: Perry Fulfs
 ; Description: Classes refresher - define classes in JavaScript
@@ -30,9 +30,33 @@ console.log(header.display("Perry", "Fulfs", "Exercise 1.3")); // updated conten
 
 // start program
 
+function CellPhone(manufacturer, model, color, price)
+{
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.color = color;
+    this.price = price;
 
+    // get info on phone
+    this.getPrice = function()
+    {
+        return this.price;
+    }
 
+    this .getModel = function()
+    {
+        return this.model;
+    }
 
+    this.getDetails = function()
+    {
+        return "Manufacturer: " + this.manufacturer + "\nModel: " + this.getModel() + 
+            "\nColor: " + this.color + "\nPrice: " + this.getPrice();
+    }
+}
+
+var cellPhone = new CellPhone("Apple", "iPhone X", "Space Gray", "599.99");
+console.log(cellPhone.getDetails());
 
 // end program
 
