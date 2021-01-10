eff355384b0d72360551ca9cf71c0b1764b7a478
Your Name <you@example.com>
2021-01-10 13:20:27 -0600

    Commit finished correct 1.4

--- week-1/fulfs-exercise-1.4.js
+++ week-1/fulfs-exercise-1.4.js
@@ -10,7 +10,7 @@
 
 var header = require('../fulfs-header.js'); // added file from root directory
 
-console.log(header.display("Perry", "Fulfs", "Exercise 1.3")); // updated content with actual name & assignment name
+console.log(header.display("Perry", "Fulfs", "Exercise 1.4")); // updated content with actual name & assignment name
 
 /*
  Expected output:
@@ -32,8 +32,59 @@ console.log(header.display("Perry", "Fulfs", "Exercise 1.3")); // updated conten
 
 // start program
 
+function Car(model)
+{
+    this.model = model;
+}
+Car.prototype.start = function()
+{
+    console.log("Car added to the racetrack!")
+}
 
+function Truck(model, year)
+{
+    this.model = model;
+    this.year = year;
+}
+Truck.prototype.start = function()
+{
+    console.log("Truck added to the racetrack!")
+}
 
+function Jeep(model, year, color)
+{
+    this.model = model;
+    this.year = year;
+    this.color = color;
+}
+Jeep.prototype.start = function()
+{
+    console.log("Jeep added to the racetrack!")
+}
 
+var racetrack = [];
+
+function driveIt(vehicle)
+{
+    vehicle.start();
+
+    racetrack.push(vehicle);
+}
+
+var celica = new Car("Celica");
+var fordF150 = new Truck("Ford F150", "2010");
+var wrangler = new Jeep("Wrangler", "2016", "White");
+
+console.log("");
+
+driveIt(celica);
+driveIt(fordF150);
+driveIt(wrangler);
+
+console.log("\n-- The following vehicles have been added to the racetrack --");
+for (var x = 0; x < racetrack.length; x++)
+{
+    console.log(racetrack[x].constructor.name + ": " + racetrack[x].model);
+}
 
 // end program
