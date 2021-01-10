e9a9cbb205336e2c21f4656d77e54d2b8e75711d
Your Name <you@example.com>
2021-01-10 12:08:54 -0600

    first commit of 1.4

---
+++ week-1/fulfs-exercise-1.4.js
@@ -0,0 +1,39 @@
+/*
+============================================
+; Title: Exercise 1.4
+; Author: Richard Krasso
+; Date: 09 Janruary 2021
+; Modified By: Perry Fulfs
+; Description: duck typing / interfaces
+;===========================================
+*/ 
+
+var header = require('../fulfs-header.js'); // added file from root directory
+
+console.log(header.display("Perry", "Fulfs", "Exercise 1.3")); // updated content with actual name & assignment name
+
+/*
+ Expected output:
+
+ FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+
+ Car added to the racetrack!
+ Truck added to the racetrack!
+ Jeep added to the racetrack!
+
+ -- The following vehicles have been added to the racetrack --
+ Truck: <Model>
+ Car: <Model>
+ Jeep: <Model>
+
+*/
+
+// start program
+
+
+
+
+
+// end program
